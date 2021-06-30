68f35314561023461ca7dfeef8be135b32354307
nanyangzi2009 <yzlsmlbx@gmail.com>
2021-06-29 23:22:39 -0400

    Files are updated from local computer and uploaded to github

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # coding-project-template
+This repo is revised by Fred Yang
+Edx.org id: nanyangzi2009
+Email: yzlsmlbx@gmail.com (account for both github and edx.org)
--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function rateChange(){
+  var r = document.getElementById("rate");
+  var v = document.getElementById("rateValue");
+  v.innerHTML = (r.value/10).toString() + "%";
 }
-        
+
+function calulateInterest(){
+  var v = document.getElementById('amount').value.toString();
+  if(v=="" || (!isFinite(v)) || parseFloat(v)<0.01) {
+    alert("Please input a correct amount!");
+    return;
+  }
+  var amount = parseInt(v);
+  var rate = document.getElementById("rate").value/10 ;
+  var today = new Date();
+  var years = Number(document.getElementById("year").value);
+  var endyear = Number(today.getFullYear()) + years;
+  var interest = amount * rate/100 * years;
+  interest = Math.round(interest *100)/100;
+  var txt = "If you deposit <mark>" + amount + "</mark>,<br>";
+  txt += "at an interest rate at <mark>" + rate + "%</mark>.<br>";
+  txt += "You will receive an amount of <mark>" + interest + "</mark>,<br>";
+  txt += "at the year <mark>" + endyear +"</mark>";
+  document.getElementById("result").innerHTML = txt;
+  return;
+}
